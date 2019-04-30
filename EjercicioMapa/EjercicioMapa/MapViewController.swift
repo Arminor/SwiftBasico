b93cd0a2149ae3b7bce3ee0323ef7292492024a6
Andrés Miguel Ríos Minor <37130954+Arminor@users.noreply.github.com>
2019-04-29 19:27:01 -0500

    Update MapViewController.swift

--- EjercicioMapa/EjercicioMapa/MapViewController.swift
+++ EjercicioMapa/EjercicioMapa/MapViewController.swift
@@ -1,6 +1,8 @@
-//
+//  Andrés Miguel Ríos Minor. Curso de Swift Básico. 
 //  MapViewController.swift
 //  EjercicioMapa
+//     Los mapas dentro del desarrollo nativo de iOS se trabajan, se desarrollan con el servicio web de AppleMaps  
+//     A nosotros nos toca utilizar las funciones y los componentes de código que nos proporciona como framework AppleMaps.
 //
 //  Created by SSiOS on 3/14/19.
 //  Copyright © 2019 SSiOS. All rights reserved.
@@ -9,10 +11,14 @@
 
 
 import UIKit
+
+
+//Importante. Se tienen que importar MapKit y CoreLocation. Ambos frameworks para crear la visualización del mapa
+//La ubicación y el rastreo de las ubicaciones, sea del dispositivo o de las coordenadas dadas.
 import MapKit
 import CoreLocation
 
-
+//Coordenadas de la ubicación elegida
 //19.295393-> Latitud
 //-99.132996-> Longuitud
 
@@ -20,38 +26,37 @@ import CoreLocation
 
 class MapViewController: UIViewController {
     
+    //Objeto de la Interfaz Gráfica. Outlet de referencia 
     @IBOutlet weak var Mapa : MKMapView!
     
     
+    //Nuestra interfaz Gráfica.
     override func viewDidLoad() {
         super.viewDidLoad()
         
+        //Atributo que hace referencia al tipo de mapa que vamos a visualizar. '.'<tipo>
         Mapa.mapType = .standard
         
-        let puntoGeográfico = CLLocationCoordinate2DMake(19.295393, -99.132996)
+        //Dos variables que vamos a utilizar como parámetros en los métodos MKCoordinateRegion y addAnnotation
+        
+        //Esta variable
+        let puntoGeografico = CLLocationCoordinate2DMake(19.295393, -99.132996)
         let texto = MKPointAnnotation()
         
-        texto.coordinate = puntoGeográfico
+        //MKPointAnnotation es una clase. texto es el identificador de nuestro objeto, es entonces nuestro objeto pues.
+        //coordinate, title, subtitle son atributos de la clase MKPointAnnotation brindada por nueestro framework MapKit
+        texto.coordinate = puntoGeografico
         texto.title = "Copacabana"
         texto.subtitle = "Los mejores desde 1875"
         
+        //Area que abarca. Mientras más se acerque al 1 más distancia abaracará ebriendo el mapa. 
         let span = MKCoordinateSpan(latitudeDelta: 0.002, longitudeDelta: 0.002)
-        let region = MKCoordinateRegion(center: puntoGeográfico, span: span)
+        let region = MKCoordinateRegion(center: puntoGeografico, span: span)
         Mapa.setRegion(region, animated: true)
         Mapa.addAnnotation(texto)
         
-        // Do any additional setup after loading the view.
+       
     }
-    
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
-    }
-    */
+ 
 
 }
